56d3c57f83a1c25d5c1e7963524f377b01244a68
Giacomo Zecchin <zecchin.giacomo93@hotmail.it>
2019-03-05 16:56:14 +0100

    add project dependencies

--- src/mirc_bot.py
+++ src/mirc_bot.py
@@ -14,6 +14,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+def error_callback(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
 def main():
     # Create the Updater and pass it your bot's token.
     updater = Updater(configurations.API_TOKEN)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # log all errors
+    dp.add_error_handler(error_callback)
+
+    # Start the Bot
+    logging.info("Starting mIRC bot...")
+    updater.start_polling()
